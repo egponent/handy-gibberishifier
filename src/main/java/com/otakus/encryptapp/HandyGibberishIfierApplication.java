e668ad8927357ff9abcf231aad3a13ab8825151e
Luka Vasko <egponent@gmail.com>
2025-02-03 09:51:27 -0500

    init commit

---
+++ src/main/java/com/otakus/encryptapp/HandyGibberishIfierApplication.java
@@ -0,0 +1,13 @@
+package com.otakus.encryptapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyGibberishIfierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyGibberishIfierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otakus/encryptapp/HandyGibberishIfierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otakus.encryptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyGibberishIfierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
