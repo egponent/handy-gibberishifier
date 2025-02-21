154af621c3ab16d23f74f85ba9df67be15bb959b
Luka Vasko <egponent@gmail.com>
2025-02-21 11:42:45 -0500

    Added reqs: Thymeleaf
    Linked html to server.

--- src/main/java/com/otakus/encryptapp/HandyGibberishIfierApplication.java
+++ src/main/java/com/otakus/encryptapp/HandyGibberishIfierApplication.java
@@ -2,12 +2,22 @@ package com.otakus.encryptapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
+@RequestMapping("/")
 public class HandyGibberishIfierApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HandyGibberishIfierApplication.class, args);
+	}
+
+	@GetMapping
+	public String displayPage() {
+		return "index";
 	}
 
 }
